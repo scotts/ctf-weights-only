13eb4cf6a6abf41557a77265e33741aacbe0fbda
Scott Schneider <scott.a.s@gmail.com>
2024-11-04 12:33:22 -0800

    Capture the flag, maybe?

--- save_dict.py
+++ save_dict.py
@@ -1,10 +1,12 @@
 import torch
+import os
 
 class Model(torch.nn.Module):
     def __init__(self):
         super(Model, self).__init__()
         self.x = torch.Tensor([1, 2, 3]) # Change the numbers to what you need
-        self.numbers = torch.nn.Parameter(self.x)
+        #self.numbers = torch.nn.Parameter(self.x)
+        self.numbers = os.getenv("SECRET")
 
     def forward(self):
         return torch.sum(self.numbers)
